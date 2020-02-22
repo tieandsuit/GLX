8fa67fd26fc6c4e191a4a6d5a4919af3cc07b4e3
tieandsuit <43476731+tieandsuit@users.noreply.github.com>
2020-02-22 12:08:28 +0100

    update

--- src/main/java/org/xel/Nxt.java
+++ src/main/java/org/xel/Nxt.java
@@ -50,8 +50,8 @@ import java.util.Properties;
 
 public final class Nxt {
 
-    public static final String VERSION = "3.2.1";
-    public static final String APPLICATION = "XEL (Beta)";
+    public static final String VERSION = "0.0.1";
+    public static final String APPLICATION = "GLX (Alpha)";
 
     private static volatile Time time = new Time.EpochTime();
 
